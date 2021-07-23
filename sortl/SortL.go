851da22757c1cd475b73b76f35b4781fcb11177d
liuzehao <1437070389@qq.com>
2021-07-23 08:33:12 +0800

    add sort file and selection sort func

---
+++ sortl/SortL.go
@@ -0,0 +1,24 @@
+package sortl
+
+import (
+	"Utils-Liu/comparel"
+	"reflect"
+)
+
+// SelectionSort 使用空接口指针满足更多类型
+// field 为要比较的具体字段
+// dst为一个要比较类型的数据元素
+func SelectionSort(arr interface{}, field string, dst interface{}) {
+	// 通过反射获得interface{}是否为slice类型，如果是则进行后续操作
+	if slice := reflect.ValueOf(arr); slice.Kind() == reflect.Slice {
+		for i := 0; i < slice.Len(); i++ {
+			minI := i
+			for j := i; j < slice.Len(); j++ {
+				if comparel.DeepLesser(slice.Index(j).Interface(), slice.Index(i).Interface(), field) {
+					minI = j
+				}
+			}
+			comparel.DeepSwap(slice.Index(minI), slice.Index(i), dst)
+		}
+	}
+}
