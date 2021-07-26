e0a694052d3f7d651649ce7760ad361797cf0ead
liuzehao <1437070389@qq.com>
2021-07-26 08:14:43 +0800

    simplify the sort func

--- sortl/SortL.go
+++ sortl/SortL.go
@@ -8,7 +8,7 @@ import (
 // SelectionSort 使用空接口指针满足更多类型
 // field 为要比较的具体字段
 // dst为一个要比较类型的数据元素
-func SelectionSort(arr interface{}, field string, dst interface{}) {
+func SelectionSort(arr interface{}, field string) {
 	// 通过反射获得interface{}是否为slice类型，如果是则进行后续操作
 	if slice := reflect.ValueOf(arr); slice.Kind() == reflect.Slice {
 		for i := 0; i < slice.Len(); i++ {
@@ -18,7 +18,7 @@ func SelectionSort(arr interface{}, field string, dst interface{}) {
 					minI = j
 				}
 			}
-			comparel.DeepSwap(slice.Index(minI), slice.Index(i), dst)
+			comparel.DeepSwap(slice.Index(minI), slice.Index(i))
 		}
 	}
 }
