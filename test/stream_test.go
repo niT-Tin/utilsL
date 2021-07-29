2fdd4ae20b9a453340339be08e9a084b4d69b8d8
liuzehao <1437070389@qq.com>
2021-07-29 13:50:09 +0800

    add the non-struct test

--- test/stream_test.go
+++ test/stream_test.go
@@ -12,20 +12,19 @@ type Person struct {
 	Age  int
 }
 
-func TestStream_Filter(t *testing.T) {
-	//s := []int{1, 2, 3, 4, 5, 6}
+var ss = []Person{
+	{"Bruce", 25},
+	{"Barry", 23},
+	{"Hal", 30},
+	{"Dianna", 24},
+	{"Ashin", 27},
+	{"Max", 21},
+	{"Mao", 20},
+	{"Test", 99},
+	{"Alex", 18},
+}
 
-	ss := []Person{
-		{"Bruce", 25},
-		{"Barry", 23},
-		{"Hal", 30},
-		{"Dianna", 24},
-		{"Ashin", 27},
-		{"Max", 21},
-		{"Mao", 20},
-		{"Test", 99},
-		{"Alex", 18},
-	}
+func TestStream_Filter(t *testing.T) {
 
 	Ns := streaml.New(ss)
 	fmt.Println(ss)
@@ -35,10 +34,30 @@ func TestStream_Filter(t *testing.T) {
 	fmt.Println("ss = ", ss)
 	fmt.Println("res = ", res)
 	fmt.Println("------------------------")
+
+	s := []int{1, 2, 3, 4, 5, 6}
+	TNs := streaml.New(s)
+	// if the slice contains the basic type the parameter of Sort and Max or Min methods
+	// should be the empty string
+	res = TNs.Filter(`a -> a >= 4`).Sort("").Max("")
+	fmt.Println(res.(int))
+	fmt.Println("------------------------")
+
+	res = TNs.Filter(`c -> c >= 3`).ToSlice()
+	fmt.Println(res)
+
+	fmt.Println(TNs.Data)
 }
+
+func TestCompare_NewAssign(t *testing.T) {
+	testFunc(ss)
+}
+
 func testFunc(a interface{}) {
 	fmt.Println("Type of a", reflect.TypeOf(a))
 	value := reflect.ValueOf(a)
-	fmt.Println(reflect.ValueOf(value.Index(0)))
-	fmt.Println(value.Index(0))
+	of := reflect.ValueOf(value.Index(0))
+	fmt.Println(of)
+	//fmt.Println(comparel.NewAssign(value.Index(0).Interface()).UnsafeAddr())
+	//fmt.Println(value.Index(0).UnsafeAddr())
 }
